b49f775f8c2cae2ae27ce840889f367a455077c7
Caio Rodrigues <caiorodrigues@hotmail.com>
2019-05-01 22:50:31 -0300

    GoBarber - Logout

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const session = require('express-session')
-const LokiStore = require('connect-loki')(session)
+const FileStore = require('session-file-store')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
+const flash = require('connect-flash')
 
 class App {
   constructor () {
@@ -16,13 +17,15 @@ class App {
 
   middlewares () {
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(flash())
     this.express.use(
       session({
-        store: new LokiStore({
+        name: 'root',
+        secret: 'MyAppSecret',
+        resave: true,
+        store: new FileStore({
           path: path.resolve(__dirname, '..', 'tmp', 'sessions.db')
         }),
-        secret: 'MyAppSecret',
-        resave: false,
         saveUninitialized: true
       })
     )
